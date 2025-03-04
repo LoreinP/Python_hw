2e9917493cc3343e71ee97aee03b7aa16db27953
LoreinP <Lari88356@gmail.com>
2025-03-04 22:13:04 +1000

    #Lesson_5 modal window

--- 05_lesson/FF_modal window.py
+++ 05_lesson/FF_modal window.py
@@ -10,21 +10,20 @@ from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
+
 # открыть страницу
-driver.get("http://the-internet.herokuapp.com/entry_ad/.")
+driver.get("http://the-internet.herokuapp.com/entry_ad")
+
 
 sleep(5)
+
 
 # нажать на кнопку Close
 close_button = driver.find_element(By.CSS_SELECTOR, ".modal-footer .close-button")
 close_button.click()
 
+
 sleep(5)
-
-alert = Alert(driver)
-alert.accept()
-
-sleep(10)
 
 driver.quit()
 
