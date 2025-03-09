2b458a189954a9e4cd7073c398a6b6132a31a5e7
LoreinP <Lari88356@gmail.com>
2025-03-09 21:02:56 +1000

    #Lesson_6

---
+++ 06_Lesson/Rename_button.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.get("http://uitestingplayground.com/textinput")
+
+
+input = driver.find_element(By.CSS_SELECTOR,'.form-control')
+input.send_keys('SkyPro')
+
+button = driver.find_element(By.CSS_SELECTOR, '#updatingButton').click()
+#name = button.find_element(By.CSS_SELECTOR, "innerText")
+
+print(button)
