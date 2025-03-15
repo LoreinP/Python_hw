574a527d2619f4d3a53155b60a0d26e885ef308f
LoreinP <Lari88356@gmail.com>
2025-03-16 09:34:51 +1000

    #Lesson_6

--- 06_Lesson/click on button.py
+++ 06_Lesson/click on button.py
@@ -1,19 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 
-chrome = webdriver.Firefox()
-wait = WebDriverWait(chrome, 20)
+driver = webdriver.Chrome(
+    service=ChromeService(ChromeDriverManager().install()))
 
-chrome.get("http://uitestingplayground.com/ajax")
+driver.get("http://uitestingplayground.com/ajax")
 
+driver.implicitly_wait(20)
 
-click_button = chrome.find_element(By.CSS_SELECTOR, ('button[class="btn btn-primary"]')).click()
-green_text = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".bg-success"))).text
+driver.find_element(By.ID, "ajaxButton").click()
 
-print(green_text)
+element = driver.find_element(By.CSS_SELECTOR, "p.bg-success")
 
+print(element.text)
 
-chrome.quit()
-
+driver.quit()
