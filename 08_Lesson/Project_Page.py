acc90a56b0263c0ab2742375ca0cb77089f58b06
LoreinP <Lari88356@gmail.com>
2025-04-20 17:55:14 +1000

    Lesson8

--- 08_Lesson/Project_Page.py
+++ 08_Lesson/Project_Page.py
@@ -2,10 +2,18 @@ import requests
 
 
 class ProjectPage:
-    BASE_URL = "http://api-v2/projects"
+    BASE_URL = "https://ru.yougile.com/api-v2/projects"
     def create_project(self, project):
         response = requests.post(f"{self.BASE_URL}/users", json={"project": project})
         return response
+
+    def get_token(user='harrypotter', password='expelliarmus'):
+        creds = {
+        'username': user,
+        'password': password
+        }
+        resp = requests.post(BASE_URL + '/auth/login', json=creds)
+        return resp.json()["user_token"]
 
     def authenticate_project(self, project):
         response = requests.post(f"{self.BASE_URL}/auth", json={"project": project})
